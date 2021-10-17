0970a022ca3b3581d01f9eddd7071a661755a767
Juliana Rodionova <90824067+MarineWaters@users.noreply.github.com>
2021-10-17 08:15:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите оценку (от 1 до 10)");
+                    var n = int.Parse(Console.ReadLine());
+                    if (n > 10 || n < 1)
+                        throw new Exception();
+                    if (n == 1 || n == 2 || n == 3)
+                        Console.WriteLine("Неудовлетворительно");
+                    else if (n == 4 || n == 5)
+                        Console.WriteLine("Удовлетворительно");
+                    else if (n == 6 || n == 7)
+                        Console.WriteLine("Хорошо");
+                    else
+                        Console.WriteLine("Отлично");
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Неверный ввод.");
+                }
+            }
+        }
+    }
+}
