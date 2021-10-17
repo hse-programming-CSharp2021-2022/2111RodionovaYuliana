6a96f83e8cad8cd9a82ab13f0e8313081c1c8c4e
Juliana Rodionova <90824067+MarineWaters@users.noreply.github.com>
2021-10-17 08:23:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+// задача 2 из презентации
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool Shift(ref char ch)
+        {
+            string alph = "abcdefghijklmnopqrstuvwxyz";
+            if (alph.Contains(ch))
+            {
+                int k = alph.IndexOf(ch);
+                if (k + 4 >= alph.Length)
+                    ch = alph[(k + 4) % alph.Length];
+                else
+                    ch = alph[k + 4];
+                return true;
+            }
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите один символ.");
+                    var a = Console.ReadLine();
+                    if (a.Length > 1)
+                        throw new Exception();
+                    char i = a[0];
+                    Console.WriteLine(Shift(ref i));
+                    if (i != a[0])
+                        Console.WriteLine(i);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                }
+            }
+        }
+    }
+}
