4f3792d9db04c8692999c54c48ff3006bbac5128
Juliana Rodionova <90824067+MarineWaters@users.noreply.github.com>
2021-10-17 08:27:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+// возможно, предполагалось, что в этой задаче массив вводится пользователем, ноо я решила, что буду генерировать его сама. И искать элементы, которые повторяются ровно дважды.
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    var arr = new int[100];
+                    Random rand = new();
+                    for (int i = 0; i < 100; i++)
+                    {
+                        arr[i] = rand.Next(1, 101);
+                        Console.Write(arr[i] + " ");
+                    }
+                    Console.WriteLine("\nЭлементы, повторяющиеся дважды: ");
+                    for (int j = 0; j < 100; j++)
+                        if (arr.Count(u => u == j) == 2)
+                            Console.Write(j + " ");
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            }
+        }
+    }
+}
