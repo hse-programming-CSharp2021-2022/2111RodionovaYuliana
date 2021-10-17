f5ab96b3936132451e57dbc5f3f4a3fe341d4131
Juliana Rodionova <90824067+MarineWaters@users.noreply.github.com>
2021-10-17 08:14:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        void Reverse(ref string s)
+        {
+            int i = s.Length - 1;
+            if (s != "0")
+                while (s[i] == '0')
+                {
+                    s = s.Remove(i);
+                    i--;
+                }
+            for (int j = s.Length - 1; j >= 0; j--)
+                Console.Write(s[j]);
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите целое число.");
+                    var a = long.Parse(Console.ReadLine());
+                    string s = a.ToString();
+                    Reverse(ref s);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Неверный ввод.");
+                }
+            }
+        }
+    }
+}
