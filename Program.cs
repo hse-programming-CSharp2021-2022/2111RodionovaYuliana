71b7385af4c3152c4940e7bc737e38738f759e68
Juliana Rodionova <90824067+MarineWaters@users.noreply.github.com>
2021-10-17 08:29:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите размер массива. ");
+                    var n = uint.Parse(Console.ReadLine());
+                    Random rand = new();
+                    var arr = new double[n];
+                    var arr1 = new double[n];
+                    var arr2 = new double[n];
+                    for (int i = 0; i < n; i++)
+                    {
+                        arr[i] = rand.NextDouble();
+                        Console.Write(arr[i] + " ");
+                        arr1[i] = Math.Truncate(arr[i]);
+                        arr2[i] = arr[i] - arr1[i];
+                    }
+                    Console.WriteLine("\nЦелая и дробная части: ");
+                    for (int j = 0; j < n; j++)
+                        Console.Write(arr1[j] + "     " + arr2[j]);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                }
+            }
+        }
+    }
+}
