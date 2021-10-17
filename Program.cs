bcb36d98c85990170ab12a42b1ae3e811e03aa8b
Juliana Rodionova <90824067+MarineWaters@users.noreply.github.com>
2021-10-17 08:22:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// Из условия не очень понятно, нужна там таблица значений или сумма, так что вывела сумму.
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static double Sum(uint n)
+        {
+            double s = 0;
+            for (int i = 1; i <= n; i++)
+                s += (i + 0.3) / ((3 * i * i) + 5);
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите целое неотрицательное число.");
+                    Console.WriteLine(Sum(uint.Parse(Console.ReadLine())));
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод.");
+                }
+            }
+        }
+    }
+}
