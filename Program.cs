2eb88791b2cda5835148f7f1ce7ba5333a738226
Juliana Rodionova <90824067+MarineWaters@users.noreply.github.com>
2021-10-17 08:13:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp16091
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите целое число.");
+                    var num = long.Parse(Console.ReadLine());
+                    if (num < 0)
+                        num *= -1;
+                    string s = num.ToString();
+                    int maxi = 0; int mini = 9;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (s.Contains(i.ToString()))
+                        {
+                            if (i > maxi)
+                                maxi = i;
+                            if (i < mini)
+                                mini = i;
+                        }
+                    }
+                    Console.WriteLine(mini + " " + maxi); ;
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+                }
+            }
+        }
+    }
+}
